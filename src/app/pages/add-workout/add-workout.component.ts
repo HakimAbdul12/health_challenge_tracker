d625fabbba7283be4b579905d80720b19ed90832
HakimAbdul12 <abdulhakimaben@gmail.com>
2024-07-28 07:52:45 +0000

    Add workout details page and update the format-workout pipe

--- src/app/pages/add-workout/add-workout.component.ts
+++ src/app/pages/add-workout/add-workout.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { WorkoutService } from '../../services/workout/workout.service';
 import { CommonModule } from '@angular/common';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-add-workout',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, ToastModule],
   templateUrl: './add-workout.component.html',
-  styleUrl: './add-workout.component.css'
+  styleUrl: './add-workout.component.css',
+  providers: [MessageService],
 })
 export class AddWorkoutComponent {
 
   applyForm!: FormGroup;
 
-  constructor(private workoutService: WorkoutService) { }
+  constructor(private workoutService: WorkoutService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.applyForm = new FormGroup({
@@ -30,5 +33,10 @@ export class AddWorkoutComponent {
     } else {
       return
     }
+  }
+
+  show() {
+    console.log('must show')
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
   }
 }
--- src/app/pipes/format-workouts.pipe.ts
+++ src/app/pipes/format-workouts.pipe.ts
@@ -7,7 +7,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatWorkoutsPipe implements PipeTransform {
 
   transform(workouts: any[]): string {
-    return workouts.map(workout => `${workout.type}`).join(', ');
+    // Create a map to count occurrences of each workout type
+    const typeCountMap = new Map<string, number>();
+
+    // Count each workout type
+    workouts.forEach(workout => {
+      const type = workout.type;
+      if (typeCountMap.has(type)) {
+        typeCountMap.set(type, typeCountMap.get(type)! + 1);
+      } else {
+        typeCountMap.set(type, 1);
+      }
+    });
+
+    // Convert the map to a formatted string
+    return Array.from(typeCountMap.entries())
+      .map(([type, count]) => count > 1 ? `${type}(${count})` : type)
+      .join(', ');
   }
+
 
 }
